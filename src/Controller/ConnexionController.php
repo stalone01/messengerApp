16792d413e8fae39221fae1a2fbe0e83fdd1f619
stalone01 <rasoamampioninastalone@gmail.com>
2024-08-08 17:43:34 +0300

    feat: creation de formulaire de connexion

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class ConnexionController extends AbstractController
+{
+    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
+    public function connexion(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+
+        return $this->render('connexion/index.html.twig', [
+            'last_username' => '$lastUsername',
+            'error' => '$error'
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): void
+    {
+        //
+    }
+}
