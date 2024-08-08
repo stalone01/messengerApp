3400d477b6aeac1f0a56292bb3451906bc8dbd7a
stalone01 <rasoamampioninastalone@gmail.com>
2024-08-08 11:30:33 +0300

    feat: mise en place de server cboden/rachet

---
+++ bin/websocket-server.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Ratchet\Server\IoServer;
+use App\WebSocket\ChatHandler;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new ChatHandler()
+        )
+        ),
+    8080
+);
+
+$server->run();
