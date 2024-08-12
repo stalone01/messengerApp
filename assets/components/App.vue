5eaf7e982461a9497099b685b70b7e5c885e33ef
stalone01 <rasoamampioninastalone@gmail.com>
2024-08-12 15:30:13 +0300

    feat: ajout de websocket server pour le frontend

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,16 +1,56 @@
-<template>
-    <div id="app">
-      <h1>Hello Vue in Symfony</h1>
+
+   <template>
+    <div>
+      
+      
+    </div>
+    <div  class="example-wrapper">
+
+      <h1>Conversation...</h1>
+
+      <div class="chat-container">
+          <div v-for="message in messages" :key="message">{{ message }}</div>
+          <div class="chat-box" id="chatBox">
+              <div class="message received">
+                  <p>Bonjour! Comment puis-je vous aider?</p>
+              </div>
+              <div class="message send">
+                  <p>Bonjour!!!</p>
+              </div>
+          </div>
+
+          <form id="messageForm">
+              <input v-model="newMessage" @keyup.enter="sendMessage" type="text" id="messageInput" placeholder="Écrivez votre message..." required/>
+              <button type="submit">Envoyer</button>
+          </form>
+      </div>
+
     </div>
   </template>
   
   <script>
   export default {
-    name: 'App'
+    data() {
+      return {
+        socket: null,
+        messages: [],
+        newMessage: '',
+      };
+    },
+    created() {
+      this.socket = new WebSocket('ws://localhost:9502'); // URL de votre serveur WebSocket
+      this.socket.onmessage = (event) => {
+        this.messages.push(event.data);
+      };
+    },
+    methods: {
+      sendMessage() {
+        if (this.newMessage.trim()) {
+          this.socket.send(this.newMessage);
+          this.newMessage = '';
+        }
+      },
+    },
   };
   </script>
-  
-  <style scoped>
-  /* Styles spécifiques à ce composant */
-  </style>
   
